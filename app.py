95f5a305d516f19901d1a49620a9eaf22b3ca408
OmarKhaledAbdlhafez <43797108+OmarKhaledAbdlhafez@users.noreply.github.com>
2020-04-06 00:04:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,9 @@ def predict():
     output = round(prediction[0], 2)
 
     return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
-
+@app.route('/test',methods=['POST'])
+def test():
+    return "it work";
 
 if __name__ == "__main__":
     app.run(debug=True)
